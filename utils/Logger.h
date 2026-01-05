1aca6c9932fe4e005f5e76d4647d3e04769a01bb
Anuj Yadav <anujyadav47247@gmail.com>
2026-01-05 11:43:27 +0530

    Enhance logger utility

--- utils/Logger.h
+++ utils/Logger.h
@@ -2,11 +2,20 @@
 #define LOGGER_H
 
 #include <iostream>
+#include <string>
 
 class Logger {
 public:
     static void info(const std::string& msg) {
         std::cout << "[INFO] " << msg << std::endl;
+    }
+
+    static void warn(const std::string& msg) {
+        std::cout << "[WARN] " << msg << std::endl;
+    }
+
+    static void error(const std::string& msg) {
+        std::cerr << "[ERROR] " << msg << std::endl;
     }
 };
 
